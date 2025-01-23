f44f27d9c58a6403381c1c2275def8445e4a90a3
Gaurav Singh <gp5in9h@gmail.com>
2025-01-23 23:06:30 +0530

    tagger completed

--- RAG/utils.py
+++ RAG/utils.py
@@ -1,5 +1,9 @@
 from transformers import AutoTokenizer
 from adapters import AutoAdapterModel
+from semantic_text_splitter import TextSplitter
+from os import listdir
+from os.path import isfile, join
+
 
 import chromadb
 from chromadb.config import Settings
@@ -7,13 +11,33 @@ from chromadb.api.types import QueryResult
 from transformers.utils import logging
 import warnings
 
+import ollama
+
+from pydantic import BaseModel
+
+
 logging.set_verbosity(40)
 warnings.filterwarnings("ignore", category=FutureWarning)
+
+
+tag_generation_prompt = """
+You are a summary and tags generator,
+your role is to look at the document provided,
+and generate category tags associated to it,
+along with a 5 line summary,
+you should limit the number of tags to 3.
+"""
+
+
+class TagsModel(BaseModel):
+    tags: list[str]
+    summary: str
 
 
 class RAGtoolkit:
     def __init__(self, collection_name="generic"):
         settings = Settings(is_persistent=True)
+        self.splitter = TextSplitter(overlap=True, capacity=1000, trim=True)
         self.tokenizer = AutoTokenizer.from_pretrained("allenai/specter2_base")
         self.model = AutoAdapterModel.from_pretrained("allenai/specter2_base")
         self.model.load_adapter(
@@ -21,6 +45,7 @@ class RAGtoolkit:
         )
         self.client = chromadb.Client(settings=settings)
         self._collection = self.client.get_or_create_collection(name=collection_name)
+        self.ollama_client = ollama.Client()
 
     def _generate_embeddings(self, inputs: list[str]):
         inputs = self.tokenizer(
@@ -35,6 +60,37 @@ class RAGtoolkit:
         # take the first token in the batch as the embedding
         embeddings = output.last_hidden_state[:, 0, :].tolist()
         return embeddings
+
+    def generate_chunks(self, doc: str) -> list[str]:
+        return self.splitter.chunks(doc)
+
+    def generate_meta(self, docs_dir: str) -> list[str]:
+        tags = set()
+        files = [
+            join(docs_dir, f) for f in listdir(docs_dir) if isfile(join(docs_dir, f))
+        ]
+        summarys = []
+        for file in files:
+            with open(file, "r", encoding="utf-8") as f:
+                res = self.ollama_client.chat(
+                    model="deepseek-r1",
+                    messages=[
+                        {"role": "system", "content": tag_generation_prompt},
+                        {"role": "user", "content": f.read()},
+                    ],
+                    format=TagsModel.model_json_schema(),
+                    options={"num_ctx": 8192},
+                )
+                print(res["message"]["content"])
+                page_level_meta = TagsModel.model_validate_json(
+                    res["message"]["content"]
+                )
+                print(page_level_meta)
+                for tag in page_level_meta.tags:
+                    tags.add(tag)
+                summarys.append(page_level_meta.summary)
+
+        return {"tags": list(tags), "summarys": summarys}
 
     def add_docs(self, docs: list[str]):
         self._collection.upsert(
@@ -51,13 +107,6 @@ class RAGtoolkit:
         return res
 
 
-from semantic_text_splitter import TextSplitter
+kit = RAGtoolkit()
 
-
-splitter = TextSplitter(overlap=True, capacity=1000, trim=True)
-
-chunks = splitter.chunks(
-    "A simple diagram showing a switch with an OFF and ON position. The OFF position is on the left, labeled 'OFF', while the ON position is on the right, labeled 'ON'. Arrows indicate that pressing or tapping the switch will change its state from OFF to ON."
-)
-
-print(chunks)
+print(kit.generate_meta("outputs/pages/chapter_0"))
--- extraction/pipeline.py
+++ extraction/pipeline.py
@@ -13,7 +13,7 @@ from PIL import Image
 from pdf2image import convert_from_path
 
 
-def extraction_pipeline(input_img: str, page_num: int):
+def extraction_pipeline(input_img: str, chapter_num: int, page_num: int):
     # 1) Load the pre-trained model predictions
     det_res = model.predict(
         input_img,  # Image to predict
@@ -125,8 +125,17 @@ def extraction_pipeline(input_img: str, page_num: int):
     os.makedirs("outputs", exist_ok=True)
     if not os.path.exists(os.path.join("outputs", "images")):
         os.makedirs(os.path.join("outputs", "images"))
+        if not os.path.exists(
+            os.path.join("outputs", "images", f"chapter_{chapter_num}")
+        ):
+            os.makedirs(os.path.join("outputs", "images", f"chapter_{chapter_num}"))
+
     if not os.path.exists(os.path.join("outputs", "pages")):
         os.makedirs(os.path.join("outputs", "pages"))
+        if not os.path.exists(
+            os.path.join("outputs", "pages", f"chapter_{chapter_num}")
+        ):
+            os.makedirs(os.path.join("outputs", "pages", f"chapter_{chapter_num}"))
 
     for idx, det in enumerate(all_detections):
         # Crop the snippet
@@ -143,7 +152,9 @@ def extraction_pipeline(input_img: str, page_num: int):
         #
         if det.class_id == 3:  # image data
             description = extract_image(det.file_location)
-            save_path = f"outputs/images/page_{page_num}_{idx}.jpg"
+            save_path = (
+                f"outputs/images/chapter_{chapter_num}/page_{page_num}_{idx}.jpg"
+            )
             print(f"image {save_path} - {idx} - {description}")
             shutil.copyfile(src=det.file_location, dst=save_path)
             content += (
@@ -222,7 +233,11 @@ def extraction_pipeline(input_img: str, page_num: int):
             )
 
     # 7) Write the page's content to disk
-    with open(f"outputs/pages/page_{page_num + 1}.txt", "w", encoding="utf-8") as f:
+    with open(
+        f"outputs/pages/chapter_{chapter_num}/page_{page_num + 1}.txt",
+        "w",
+        encoding="utf-8",
+    ) as f:
         f.write(content)
 
     # Clean up /temp
@@ -248,7 +263,7 @@ def extraction_pipeline_from_pdf(pdf_path: str) -> str:
         page.save(page_filename, "PNG")
 
         # Call the pipeline for this image
-        page_content = extraction_pipeline(page_filename, page_num=i)
+        page_content = extraction_pipeline(page_filename, chapter_num=0, page_num=i)
         all_pages_content.append(f"--- Page {i+1} ---\n{page_content}")
 
     # Combine text from all pages
